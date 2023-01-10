05ec5728d4c681af6c811789f8cd55e05267bdb8
Ubertronian04 <S10223002@connect.np.edu.sg>
2023-01-11 00:16:36 +0800

    Wizard v1

--- Wizard_TeamA (REF).py
+++ Wizard_TeamA (REF).py
@@ -27,12 +27,10 @@ class Wizard_TeamA(Character):
 
         seeking_state = WizardStateSeeking_TeamA(self)
         attacking_state = WizardStateAttacking_TeamA(self)
-        fleeing_state = WizardStateFleeing_TeamA(self)
         ko_state = WizardStateKO_TeamA(self)
 
         self.brain.add_state(seeking_state)
         self.brain.add_state(attacking_state)
-        self.brain.add_state(fleeing_state)
         self.brain.add_state(ko_state)
 
         self.brain.set_state("seeking")
@@ -69,34 +67,22 @@ class WizardStateSeeking_TeamA(State):
             self.wizard.velocity.normalize_ip();
             self.wizard.velocity *= self.wizard.maxSpeed
 
+    def check_conditions(self):
+
         # check if opponent is in range
         nearest_opponent = self.wizard.world.get_nearest_opponent(self.wizard)
         if nearest_opponent is not None:
             opponent_distance = (self.wizard.position - nearest_opponent.position).length()
-            self.wizard.target = nearest_opponent
-            if self.wizard.current_ranged_cooldown <= 0 and opponent_distance <= self.wizard.min_target_distance:
-                self.wizard.ranged_attack(self.wizard.target.position, self.wizard.explosion_image)
-
-    def check_conditions(self):
-
+            if opponent_distance <= self.wizard.min_target_distance:
+                    self.wizard.target = nearest_opponent
+                    return "attacking"
+        
         if (self.wizard.position - self.wizard.move_target.position).length() < 8:
 
             # continue on path
             if self.current_connection < self.path_length:
                 self.wizard.move_target.position = self.path[self.current_connection].toNode.position
                 self.current_connection += 1
-
-        # check if opponent is in range
-        nearest_opponent = self.wizard.world.get_nearest_opponent(self.wizard)
-
-        if nearest_opponent is not None:
-            opponent_distance = (self.wizard.position - nearest_opponent.position).length()
-            if nearest_opponent.name == "orc" and opponent_distance <= 50:
-                self.wizard.target = nearest_opponent
-                return "fleeing"
-##            if opponent_distance <= self.wizard.min_target_distance:
-##                self.wizard.target = nearest_opponent
-##                return "attacking"
             
         return None
 
@@ -111,10 +97,7 @@ class WizardStateSeeking_TeamA(State):
         
         self.path_length = len(self.path)
 
-        if (self.path_length > 1):
-            self.current_connection = 0
-            self.wizard.move_target.position = self.path[1].fromNode.position
-        elif (self.path_length > 0):
+        if (self.path_length > 0):
             self.current_connection = 0
             self.wizard.move_target.position = self.path[0].fromNode.position
 
@@ -135,13 +118,7 @@ class WizardStateAttacking_TeamA(State):
 
         # opponent within range
         if opponent_distance <= self.wizard.min_target_distance:
-            if self.wizard.target.name == "base" or self.wizard.target.name == "tower":
-                self.wizard.velocity = Vector2(0, 0)
-            else:
-                self.wizard.velocity = self.wizard.move_target.position - self.wizard.position
-                self.wizard.velocity.normalize_ip()
-                self.wizard.velocity *= self.wizard.maxSpeed
-            
+            self.wizard.velocity = Vector2(0, 0)
             if self.wizard.current_ranged_cooldown <= 0:
                 self.wizard.ranged_attack(self.wizard.target.position, self.wizard.explosion_image)
 
@@ -151,55 +128,6 @@ class WizardStateAttacking_TeamA(State):
                 self.wizard.velocity.normalize_ip();
                 self.wizard.velocity *= self.wizard.maxSpeed
 
-    def check_conditions(self):
-
-        opponent_distance = (self.wizard.position - self.wizard.target.position).length()
-
-        # target is gone
-        if self.wizard.world.get(self.wizard.target.id) is None or self.wizard.target.ko:
-            self.wizard.target = None
-            return "seeking"
-
-        if opponent_distance > self.wizard.min_target_distance:
-            self.wizard.target = None
-            return "seeking"
-
-        if (self.wizard.move_target.position - self.wizard.position).length() < 8:
-            self.wizard.velocity = Vector2(0, 0)
-            return "seeking"
-
-        nearest_opponent = self.wizard.world.get_nearest_opponent(self.wizard)
-        if nearest_opponent is not None:
-            opponent_distance = (self.wizard.position - nearest_opponent.position).length()
-            if nearest_opponent.name == "orc" and opponent_distance <= 50:
-                self.wizard.target = nearest_opponent
-                return "fleeing"
-            
-        return None
-
-    def entry_actions(self):
-
-        return None
-
-class WizardStateFleeing_TeamA(State):
-
-    def __init__(self, wizard):
-
-        State.__init__(self, "fleeing")
-        self.wizard = wizard
-
-        self.wizard.path_graph = self.wizard.world.paths[randint(0, len(self.wizard.world.paths)-1)]
-        
-
-    def do_actions(self):
-
-        self.wizard.velocity = self.wizard.move_target.position - self.wizard.position
-        if self.wizard.velocity.length() > 0:
-            self.wizard.velocity.normalize_ip();
-            self.wizard.velocity *= self.wizard.maxSpeed
-
-        if self.wizard.current_ranged_cooldown <= 0:
-            self.wizard.ranged_attack(self.wizard.target.position, self.wizard.explosion_image)
 
     def check_conditions(self):
 
@@ -207,35 +135,13 @@ class WizardStateFleeing_TeamA(State):
         if self.wizard.world.get(self.wizard.target.id) is None or self.wizard.target.ko:
             self.wizard.target = None
             return "seeking"
-
-        if (self.wizard.position - self.wizard.move_target.position).length() < 8:
-
-            # continue on path
-            if self.current_connection < self.path_length:
-                self.wizard.move_target.position = self.path[self.current_connection].toNode.position
-                self.current_connection += 1
             
         return None
 
     def entry_actions(self):
 
-        nearest_node = self.wizard.path_graph.get_nearest_node(self.wizard.position)
+        return None
 
-        self.path = pathFindAStar(self.wizard.path_graph, \
-                                  nearest_node, \
-                                  self.wizard.path_graph.nodes[self.wizard.base.spawn_node_index])
-
-        
-        self.path_length = len(self.path)
-
-        if (self.path_length > 1):
-            self.current_connection = 0
-            self.wizard.move_target.position = self.path[1].fromNode.position
-        elif (self.path_length > 0):
-            self.current_connection = 0
-            self.wizard.move_target.position = self.path[0].fromNode.position
-        else:
-            self.wizard.move_target.position = self.wizard.path_graph.nodes[self.wizard.base.target_node_index].position
 
 class WizardStateKO_TeamA(State):
 
